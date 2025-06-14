6883729d2d5f5faae9454e75cb19e787a2a75bf3
atulyasharan29 <143794824+atulyasharan29@users.noreply.github.com>
2025-06-15 00:32:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -258,4 +258,5 @@ def sign_up():
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(port=5000,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host = '0.0.0.0', port = port, debug = True)
